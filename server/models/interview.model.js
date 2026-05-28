b3b353ae0b581902f625a3210679e68fedcd8c90
Aditya Singh <adityasinghrajput196@gmail.com>
2026-05-29 03:40:47 +0530

    feat(interview): enhance interview model with detailed question scoring and evaluation fields

---
+++ server/models/interview.model.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import { Schema } from 'mongoose';
+
+
+const questionSchema = new mongoose.Schema(
+    {
+        question: String,
+        difficulty: String,
+        timeLimit: Number,
+        answer: String,
+        feedback: String,
+
+        score: {// Add a score field to store the candidate's score for each question
+            type: Number,
+             default: 0, // Default score is 0, it can be updated after evaluating the candidate's answer
+        },
+        confidence: {// Add a confidence field to store the AI's confidence level in the candidate's answer
+            type: Number,
+            default: 0, // Default confidence is 0, it can be updated after evaluating the candidate's answer
+        },
+        communication: {// Add a communicationSkills field to store the AI's evaluation of the candidate's communication skills based on their answer
+            type: Number,
+            default: 0, // Default communication skills score is 0, it can be updated after evaluating the candidate's answer
+        }, 
+        correctness: {
+            type: Number, 
+            default: 0,
+        },
+},{ timestamps: true })
+
+
+const interviewSchema = new mongoose.Schema(
+    { 
+        // Links this entire session securely to your logged-in candidate profile in the database, ensuring that all interactions and data are properly associated with the correct user account.
+        userId: {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User",
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        experience: {
+           type: String,
+           required: true,
+        },
+        mode: {
+            type: String,
+            enum: ["HR", "Technical"], 
+            required: true,
+        },
+        resumeText: {
+            type: String,
+        },
+
+        questions: [questionSchema], // Array of questions with answers
+
+        // Final report scores (filled after interview ends)
+        finalScore: {
+            type: Number,
+            default: 0,
+        },
+
+        // Interview status
+        status: {
+            type: String,
+            enum: ["Incomplete", "completed"],
+            default: "Incomplete"
+        }
+
+},{ timestamps: true })
+
+export const Interview = mongoose.model("Interview", interviewSchema);
