404bd05ee989f4cb2bea4eea80173d529efd34e6
Aditya Singh <adityasinghrajput196@gmail.com>
2026-06-11 01:23:25 +0530

    feat(account): Implement account deletion functionality with data integrity checks

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,9 @@ import  asyncHandler  from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
+import {Interview} from "../models/interview.model.js";
+import Payment from "../models/payment.model.js";
+import { User } from "../models/user.model.js";
 
 const getCurrentUser = asyncHandler(async (req, res) => {
     if (!req.user) {
@@ -18,4 +21,35 @@ const getCurrentUser = asyncHandler(async (req, res) => {
         )
 })
 
-export  {getCurrentUser}
+const deleteAccount = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+
+    if (!userId) {
+        throw new ApiError(400, "Unauthorized: User ID missing in request context");
+    }
+
+    const deletedUser = await User.findById(userId);
+
+    if (!deletedUser) {
+        throw new ApiError(404, "User not found or already deleted");
+    }
+
+    // Cascade delete all interviews and payments associated with the user to maintain data integrity and prevent orphaned records in the database.
+    await Interview.deleteMany({ userId });
+    await Payment.deleteMany({ userId });
+    
+    // Delete the user account from the database
+    await User.findByIdAndDelete(userId);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "User account deleted successfully"
+            )
+        );
+})
+
+export  {getCurrentUser, deleteAccount}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
-import  {getCurrentUser}  from "../controllers/user.controller.js";
+import  {deleteAccount, getCurrentUser}  from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/delete-account").delete(verifyJWT, deleteAccount) //For account deletion from settings page .delete() method use because it's a destructive action
 
 export default userRouter;
